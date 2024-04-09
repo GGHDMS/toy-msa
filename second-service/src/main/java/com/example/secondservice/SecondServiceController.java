483785c8904452396798e053842c02e44300b38e
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-04-09 13:49:55 +0900

    Spring Cloud Gateway - Filter 적용

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,9 +1,12 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("second-service")
 public class SecondServiceController {
@@ -12,4 +15,9 @@ public class SecondServiceController {
         return "Welcome to the Second service.";
     }
 
+    @GetMapping("/message")
+    String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Hello World in Second Service.";
+    }
 }
