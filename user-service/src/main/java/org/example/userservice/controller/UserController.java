25b9e13beae5ee297fb136506508c4077cb1ecb1
HeoSeokMun <tjrans303@soongsil.ac.kr>
2024-04-11 16:46:34 +0900

    Users Microservice - welcome() 메소드

---
+++ user-service/src/main/java/org/example/userservice/controller/UserController.java
@@ -0,0 +1,29 @@
+package org.example.userservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.userservice.vo.Greeting;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final Environment env;
+    private final Greeting greeting;
+
+    @GetMapping("/health_check")
+    public String status() {
+        env.getProperty("greeting.message");
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+}
